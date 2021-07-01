595fa5561345fe22a4e714611d73b697a8ad5be3
Maximilian Ries <max.ries113@gmail.com>
2021-07-01 23:06:46 +0200

    Create mysecrets.py

---
+++ tldlbot/mysecrets.py
@@ -0,0 +1,9 @@
+watson_secrets = {
+  "apikey": "YOURAPIKEY",
+  "url": "https://api.eu-de.speech-to-text.watson.cloud.ibm.com/instances/YOUR INSTANCE URL",
+  "base_url": "https://api.eu-de.speech-to-text.watson.cloud.ibm.com"
+}
+
+telegram_secrets = {
+  "token": "YOUR TELEGRAM BOT TOKEN"
+}
